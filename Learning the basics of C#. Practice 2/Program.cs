b296c99d83fa1823054ea925864c2004dae34645
Чадамба Чамзы <chaamzy2@mail.ru>
2025-08-04 22:29:17 +0700

    Добавьте файлы проекта.

---
+++ Learning the basics of C#. Practice 2/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число = ");
+        double @base = double.Parse(Console.ReadLine());
+        Console.Write("Введите степень = ");
+        double pow = double.Parse(Console.ReadLine());
+
+        double result = Power(@base, pow);
+        Console.WriteLine($"Результат возведение в степень = {result}");
+
+    }
+
+    static double Power(double @base, double exponent)
+    {
+        double a = @base;
+        for (int i = 1; i < exponent; i++)
+        {
+            a = a * @base;
+        }
+        return a;
+    }
+}
