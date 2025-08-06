a1ed97f5c4162c45258956d044d22ba8daf2033d
Чадамба Чамзы <chaamzy2@mail.ru>
2025-08-06 20:34:41 +0700

    Была выполнена поставленая задача: Напиши программу, которая запрашивает у пользователя число и определяет, является ли оно простым. Создайте функцию с именем IsPrime, которая принимает целое положительное число в качестве аргумента и возвращает "простое число", если число простое, "непростое число" и  в противном случае.

--- Learning the basics of C#. Practice 2/Program.cs
+++ Learning the basics of C#. Practice 2/Program.cs
@@ -3,24 +3,41 @@
     private static void Main(string[] args)
     {
 
-        Console.Write("Введите число = ");
+        /*Console.Write("Введите число = ");
         double @base = double.Parse(Console.ReadLine());
 
         Console.Write("Введите степень = ");
         double pow = double.Parse(Console.ReadLine());
 
         double result = Power(@base, pow);
-        Console.WriteLine($"Результат возведение в степень = {result}");
+        Console.WriteLine($"Результат возведение в степень = {result}");*/
 
+        Console.Write("Введите число = ");
+        int number = int.Parse(Console.ReadLine());
+        int result = IsPrime(number);
+        
     }
 
-    static double Power(double @base, double exponent)
+    static double Power(double @base, double pow)
     {
         double a = @base;
-        for (int i = 1; i < exponent; i++)
+        for (int i = 1; i < pow; i++)
         {
             a = a * @base;
         }
         return a;
+    }
+
+    static int IsPrime(int number)
+    {
+        if (number % 2 != 0 || number == 2)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine("");
+        }
+        return number;
     }
 }
