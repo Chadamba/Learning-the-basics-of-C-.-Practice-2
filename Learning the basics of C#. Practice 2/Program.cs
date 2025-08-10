cac19ab538e47984a64a83dd62b48b5ce57c9491
Чадамба Чамзы <chaamzy2@mail.ru>
2025-08-10 13:51:27 +0700

    По заданному номеру месяца определи количество дней в этом месяце.

--- Learning the basics of C#. Practice 2/Program.cs
+++ Learning the basics of C#. Practice 2/Program.cs
@@ -10,12 +10,23 @@
         double pow = double.Parse(Console.ReadLine());
 
         double result = Power(@base, pow);
-        Console.WriteLine($"Результат возведение в степень = {result}");*/
+        Console.WriteLine($"Результат возведение в степень = {result}");
 
         Console.Write("Введите число = ");
         int number = int.Parse(Console.ReadLine());
         int result = IsPrime(number);
-        
+
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        var ageDictionary = new Dictionary<string, int>();
+        ageDictionary.Add(name, age);
+        ageDictionary.Keys.ToArray();
+        Console.WriteLine(name + " " + ageDictionary[name]);*/
+
+        Console.WriteLine("Введите число месяца = ");
+        int number = int.Parse(Console.ReadLine());
+        int result = Months(number);
+
     }
 
     static double Power(double @base, double pow)
@@ -37,6 +48,38 @@
         else
         {
             Console.WriteLine("");
+        }
+        return number;
+    }
+
+    static int Months(int number)
+    {
+        switch (number)
+        {
+            case 1: Console.WriteLine(31);
+                break;
+            case 2: Console.WriteLine(28); 
+                break;
+            case  3: Console.WriteLine(31); 
+                break;
+            case  4: Console.WriteLine(30); 
+                break;
+            case 5: Console.WriteLine(31); 
+                break;
+            case 6: Console.WriteLine(30);
+                break;
+            case 7: Console.WriteLine(31);
+                break;
+            case 8: Console.WriteLine(31);
+                break;
+            case 9: Console.WriteLine(30);
+                break;
+            case 10: Console.WriteLine(31);
+                break;
+            case 11: Console.WriteLine(30);
+                break;
+            case 12: Console.WriteLine(31);
+                break;
         }
         return number;
     }
